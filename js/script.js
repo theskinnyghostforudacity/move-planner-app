fdcfb46366864b00ef5956a323d7a53db3db5863
Luca Ricci <theskinnyghost@gmail.com>
2017-04-01 18:03:32 +0200

    Set background based on given location

--- js/script.js
+++ js/script.js
@@ -15,6 +15,30 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    /**
+     * Retrieve city and address values and then construct the request URL
+     *
+     */
+    var streetValue = $('#street').val();
+    var cityValue = $('#city').val();
+    var fullAddress = streetValue + ', ' + cityValue;
+
+    if( "" == streetValue || "" == cityValue ) {
+        alert("You must provide an address!");
+        return false;
+    }
+
+    $greeting.text('Wow! I like ' + fullAddress + ' too!');
+
+    var streetviewURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + fullAddress;
+
+    $body.append(
+        $('<img>').attr({
+            'class' : 'bgimg',
+            'src'   : streetviewURL
+        })
+    );
+
     return false;
 };
 
